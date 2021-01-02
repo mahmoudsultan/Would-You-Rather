232ee02d683d2262265d1a5424576c865640af48
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2021-01-02 20:14:14 +0200

    feat/Login page style refactor.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,31 @@
 import { useState, useCallback } from 'react';
 import { connect } from 'react-redux';
+import { Card, CardHeader, CardContent,
+         Select, MenuItem, Button, CardActions, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { setAuthUser } from '../actions/authUser';
-import { Card, CardHeader, CardContent, Select, MenuItem, Button, CardActions } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    width: '50%'
+  },
+  containerBox: {
+    display: 'flex',
+    height: '100vh',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardHeader: {
+    textAlign: 'center'
+  },
+  input: {
+    width: '100%',
+  },
+  button: {
+    width: '100%',
+  }
+});
 
 const mapStateToProps = ({ users }) => ({
   users,
@@ -12,6 +36,8 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const Login = ({ users, setAuthUser }) => {
+  const classes = useStyles();
+
   const [selectedUser, setSelectedUser] = useState(Object.keys(users)[0]);
 
   const handleChangeSelectedUser = useCallback((e) => {
@@ -27,25 +53,28 @@ const Login = ({ users, setAuthUser }) => {
   }, [selectedUser, setAuthUser]);
 
   return (
-    <Card>
-      <CardHeader title='Login' subheader='Select your user.' />
-      <CardContent>
-        <Select
-          value={selectedUser}
-          onChange={handleChangeSelectedUser}
-        >
-          {
-            Object.keys(users).map((userId) => {
-              const user = users[userId];
-              return <MenuItem key={userId} value={userId}>{user.name}</MenuItem>
-            })
-          }
-        </Select>
-      </CardContent>
-      <CardActions>
-        <Button onClick={handleSetAuthUser} variant="contained" color="primary">Select</Button>
-      </CardActions>
-    </Card>
+    <Box className={classes.containerBox} display="flex">
+      <Card className={classes.root}>
+        <CardHeader className={classes.cardHeader} title='Login' subheader='Select your user.' />
+        <CardContent>
+          <Select
+            className={classes.input}
+            value={selectedUser}
+            onChange={handleChangeSelectedUser}
+          >
+            {
+              Object.keys(users).map((userId) => {
+                const user = users[userId];
+                return <MenuItem key={userId} value={userId}>{user.name}</MenuItem>
+              })
+            }
+          </Select>
+        </CardContent>
+        <CardActions>
+          <Button onClick={handleSetAuthUser} className={classes.button} variant="contained" color="primary">Select</Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
