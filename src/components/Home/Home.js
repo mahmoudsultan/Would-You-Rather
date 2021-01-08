bf119f70b5400d71424bf40e0b2eb9c01b3367f2
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2021-01-08 16:32:03 +0200

    chore/fix paddings of Home.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,22 +23,22 @@ const Home = ({ answeredQuestions, unansweredQuestions }) => {
   const [tab, setTab] = useState(0);
 
   return (
-    <div className="w-auto min-h-screen">
-      <div className="flex justify-center flex-row">
+    <div className="w-auto min-h-screen mt-8 mb-8">
+      <div className="flex text-center justify-center flex-row">
         <div
-          className={`w-64 p-5 text-xl font-medium text-gray-600 cursor-pointer tracking-wide leading-relaxed ${tab === 0 ? 'text-purple-700 font-semibold' : '' }`}
+          className={`w-64 p-2 text-xl font-medium text-gray-600 cursor-pointer tracking-wide leading-relaxed ${tab === 0 ? 'text-purple-700 font-semibold' : '' }`}
           onClick={() => setTab(0)}
         >
           Unanswered
         </div>
         <div
-          className={`w-64 p-5 ml-10 text-xl font-medium text-gray-600 cursor-pointer tracking-wide leading-relaxed ${tab === 1 ? 'text-purple-700 font-semibold' : '' }`}
+          className={`w-64 p-2 ml-10 text-xl font-medium text-gray-600 cursor-pointer tracking-wide leading-relaxed ${tab === 1 ? 'text-purple-700 font-semibold' : '' }`}
           onClick={() => setTab(1)}
         >
           Answered
         </div>
       </div>
-      <div className="mt-5">
+      <div className="mt-2">
         <div className={`tab ${tab === 0 ? '' : 'hidden'}`}>
           <ul className="questions-list">
             { unansweredQuestions.map((questionId) => <li key={questionId}><HomeQuestionCard id={questionId} /></li>) }
